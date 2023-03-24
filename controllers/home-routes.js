219481b8e1c90a0bb833c3d2521fe9113bf59ce3
mikafeng <mikafengler@gmail.com>
2023-03-24 14:36:59 -0400

    add get profile route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
+const { Post, User } = require('../models');
 
-// This is the 'get' route 
 router.get('/', async (req, res) => {
     // Here, index.html is rendered
     res.render('homepage');
@@ -15,4 +15,22 @@ router.get('/login', (req, res) => {
     res.render('login');
 })
 
+//GET profile page for user
+router.get('/profile', async (req, res) => {
+    try{
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password']},
+            include: [{model: Post}],
+        });
+
+        // const user = userData.get({plain: true});
+
+        res.render('profile' 
+        // , { ..user,
+        // logged_in:}
+            );
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const User = require('./User');
 const Post = require('./Post');
 
-User.hasMany(Project, {
+User.hasMany(Post, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
