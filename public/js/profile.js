389335fd86027a2b9255968c27b5045d4fdbbbbc
mikafeng <mikafengler@gmail.com>
2023-03-24 15:14:25 -0400

    add profile logic in public folder

---
+++ public/js/profile.js
@@ -0,0 +1,22 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#post-name').value.trim();
+    const text = document.querySelector('#post-text').value.trim();
+
+    if (name && text) {
+        const post = await fetch('/api/post', {
+            method: 'POST',
+            body: JSON.stringify({ name, text}),
+            headers: { 'Content-Type': 'application/json'}
+        });
+
+        if(response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('failed to post to dashboard');
+        }
+    }
+};
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
