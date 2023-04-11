f36e403485edcfffbb0b412cd31ba1c072e3f545
mikafeng <mikafengler@gmail.com>
2023-04-11 11:57:50 -0400

    server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session')
@@ -14,7 +13,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const hbs = exphbs.create({helpers});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: process.env.SESSION_SECRET,
@@ -39,7 +38,7 @@ app.set('view engine', 'handlebars');
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 // Sets up the routes
 app.use(routes);
@@ -48,4 +47,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
-
